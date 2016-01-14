e151d556e60f08bdcf5b6f4ddab4bd243ccecf00
Benjamin Huang <coolconfucius@yahoo.com>
2016-01-13 18:21:27 -0800

    added js, html, and css. Can add names of contacts to the list

---
+++ app.js
@@ -0,0 +1,64 @@
+'use strict'; 
+
+$(document).ready(init); 
+
+var contacts = []; 
+
+function init() {
+  console.log("init");
+
+  loadFromStorage(); 
+  updateList();
+
+  $('#add').click(add);
+  // $('#contactList').on('dblclick', 'li', removecontact);
+}
+
+
+function add() {
+  console.log("addContact");
+  var contact = {};
+  contact.name = $('#newName').val();   
+  
+  contacts.push(contact);
+  
+  saveToStorage(); 
+  updateList(); 
+}
+
+function saveToStorage(){
+  console.log("saveToStorage");
+  localStorage.contacts = JSON.stringify(contacts); 
+}
+
+function loadFromStorage(){
+  console.log("loadFromStorage");
+  if (!localStorage.contacts) {
+    localStorage.contacts = '[]'; 
+  };
+  contacts = JSON.parse(localStorage.contacts);  
+}
+
+function updateList(){
+  console.log("updateList");
+  var $contactList = $('#body');
+  $contactList.empty(); 
+  console.log("show this");
+  var $contacts = $('<div>').addClass('container').attr('id', "body");
+  contacts.forEach(function(contact){
+    console.log("here?");
+    var $newRow = $('<div>').addClass('row item'); 
+    $newRow.append($('<div>').addClass('col-md-5 name').text(contact.name) );
+    $contacts.append($newRow); 
+  });
+  $contactList.append($contacts); 
+}
+
+
+// function removecontact(){
+//   console.log("DOUBLECLICK");
+//   var index = $(this).index(); 
+//   contacts.splice(index, 1); 
+//   updateList(); 
+//   saveToStorage(); 
+// }
